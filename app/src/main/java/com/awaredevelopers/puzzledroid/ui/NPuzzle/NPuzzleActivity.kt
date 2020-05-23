1e2a85c214815a1182bd231ea031b71f81cb74b7
Ruben <ralonsot@fp.uoc.edu>
2020-05-23 20:38:11 +0200

    Añadida SplashScreen, botones en el home fragment a puzzle activity y una prueba de gridview

--- app/src/main/java/com/awaredevelopers/puzzledroid/ui/NPuzzle/NPuzzleActivity.kt
+++ app/src/main/java/com/awaredevelopers/puzzledroid/ui/NPuzzle/NPuzzleActivity.kt
@@ -1,8 +1,10 @@
 package com.awaredevelopers.puzzledroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.ImageButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -14,6 +16,11 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.awaredevelopers.puzzledroid.ui.home.HomeFragment
+import android.widget.GridView
+import com.awaredevelopers.puzzledroid.ui.NPuzzle.ColorBaseAdapter
+import kotlinx.android.synthetic.main.activity_npuzzle.*
+
 
 class NPuzzleActivity : AppCompatActivity() {
 
@@ -21,9 +28,18 @@ class NPuzzleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_npuzzle)
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
+        // Get an instance of base adapter
+        val adapter = ColorBaseAdapter()
 
+        // Set the grid view adapter
+        grid_view.adapter = adapter
+
+        // Configure the grid view
+        grid_view.numColumns = 2
+        grid_view.horizontalSpacing = 15
+        grid_view.verticalSpacing = 15
+        grid_view.stretchMode = GridView.STRETCH_COLUMN_WIDTH
 
     }
 }
--- app/src/main/java/com/awaredevelopers/puzzledroid/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/awaredevelopers/puzzledroid/ui/home/HomeViewModel.kt
@@ -3,7 +3,6 @@ package com.awaredevelopers.puzzledroid.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.awaredevelopers.puzzledroid.FooJava
 import com.awaredevelopers.puzzledroid.MainActivity
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -12,14 +11,6 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeViewModel : ViewModel() {
 
-    //TODO Quitar esta instancia a Firebase...
-    var db = FirebaseFirestore.getInstance()
 
-    private val _text = MutableLiveData<String>().apply {
-
-        var objectJava = FooJava("pos un string", 12, true, FooJava.RANDOMDES.DES1)
-        value = objectJava.toString()
-    }
-    val text: LiveData<String> = _text
 
 }
