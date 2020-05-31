9593e6c54929b10543b2cabfd127e47732f37b15
Javier López (WP) <jlopez@omitsis.com>
2020-05-31 02:44:03 +0200

    Create file

---
+++ app/src/main/java/com/awaredevelopers/puzzledroid/utils/CreateImageSlicedRandomSort.kt
@@ -0,0 +1,11 @@
+package com.awaredevelopers.puzzledroid.utils
+
+import android.graphics.Bitmap
+
+class CreateImageSlicedRandomSort {
+    open fun setRandomSort(bmp: Map<String, Bitmap>): Map<String, Bitmap> {
+        val mutableMap: MutableMap<String, Bitmap> = mutableMapOf<String, Bitmap>()
+
+        return mutableMap
+    }
+}
