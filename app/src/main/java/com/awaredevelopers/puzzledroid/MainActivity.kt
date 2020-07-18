83e886c50d0bdc8622df04841ad8404a25390038
Ruben <ralonsot@fp.uoc.edu>
2020-07-18 12:26:36 +0200

    Gallery and cam

--- app/src/main/java/com/awaredevelopers/puzzledroid/MainActivity.kt
+++ app/src/main/java/com/awaredevelopers/puzzledroid/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.awaredevelopers.puzzledroid
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -15,12 +21,19 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.*
+import com.awaredevelopers.puzzledroid.model.NPuzzleGallery
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val permisoLectura = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    private val permisoEscritura = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    private val permisoCamara = android.Manifest.permission.CAMERA
 
+    private val CODIGO_SOLICITUD_PERMISOS = 1007
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val REQUEST_GALLERY = 1001
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
@@ -36,8 +49,52 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_scores, R.id.nav_settings, R.id.nav_help), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+
+        if (validarPermisosLectura()&& validarPermisosEscritura()&& validarPermisosCamara()){
+          println("Permisos concedidos")
+        }
+        else{
+            pedirPermisos()
+        }
+
+
     }
 
+    private fun validarPermisosLectura():Boolean{
+        val hayPermisoRead = ActivityCompat.checkSelfPermission(this,permisoLectura)== PackageManager.PERMISSION_GRANTED
+        return hayPermisoRead
+    }
+
+    private fun validarPermisosEscritura():Boolean{
+        val hayPermisoWrite = ActivityCompat.checkSelfPermission(this,permisoEscritura)== PackageManager.PERMISSION_GRANTED
+        return hayPermisoWrite
+    }
+
+    private fun validarPermisosCamara():Boolean{
+        val hayPermisoCamara = ActivityCompat.checkSelfPermission(this,permisoCamara)== PackageManager.PERMISSION_GRANTED
+        return hayPermisoCamara
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun pedirPermisos(){
+        val contextoRead = ActivityCompat.shouldShowRequestPermissionRationale(this,permisoLectura)
+        val contextoWrite = ActivityCompat.shouldShowRequestPermissionRationale(this,permisoEscritura)
+        val contextoCamara = ActivityCompat.shouldShowRequestPermissionRationale(this,permisoCamara)
+        if (contextoRead || contextoWrite || contextoCamara){
+            Toast.makeText(this, "PuzzleDroid necesita acceder a la galeria y a la camara para añadir nuevos puzzles", Toast.LENGTH_SHORT).show()
+            solicitudPermisos()
+        }
+        else{
+            solicitudPermisos()
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun solicitudPermisos(){
+        requestPermissions(arrayOf(permisoLectura,permisoEscritura,permisoCamara),CODIGO_SOLICITUD_PERMISOS)
+
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
@@ -63,6 +120,25 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
+    override fun onRequestPermissionsResult(
+                requestCode: Int,
+                permissions: Array<out String>,
+                grantResults: IntArray
+            ) {
 
+                when (requestCode) {
+                    CODIGO_SOLICITUD_PERMISOS -> {
+                        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                                    grantResults[2] == PackageManager.PERMISSION_GRANTED
+                        ) {
+                            //permission from popup granted
+                        } else {
+                            //permission from popup denied
+                            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
 
 }
--- app/src/main/java/com/awaredevelopers/puzzledroid/model/NPuzzleCam.kt
+++ app/src/main/java/com/awaredevelopers/puzzledroid/model/NPuzzleCam.kt
@@ -1,16 +1,58 @@
 package com.awaredevelopers.puzzledroid.model
 
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
 import com.awaredevelopers.puzzledroid.utility.ImageUtil.getListImageSliced
 import com.awaredevelopers.puzzledroid.utility.NPuzzlePortion
 
 class NPuzzleCam(applicationContext: Context): NPuzzle(applicationContext, GameMode.CAM_PICTURE_IMG) {
 
     override fun createNPuzzlePortions(): List<NPuzzlePortion> {
-        val bmp = BitmapFactory.decodeStream(
-            context.assets.open("preloaded_npuzzle_img/ny_west_44th_street.jpg")
-        )
+
+
+        openCamera()
         return getListImageSliced(bmp, cols, rows)
     }
-}
+    companion object {
+        lateinit var imageCamera : Uri
+
+        //Permission code
+        private val REQUEST_CAMERA= 1002
+
+        lateinit var bmp : Bitmap
+    }
+
+
+        private fun openCamera(){
+
+        /*    val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, "New Picture")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera")
+            imageCamera = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)!!*/
+
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageCamera)
+            startActivityForResult(cameraIntent,REQUEST_CAMERA, Bundle.EMPTY)
+
+        }
+
+
+        //handle result of picked image
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                 bmp = BitmapFactory.decodeFile(data?.data.toString())
+            }
+        }
+    }
+
+
